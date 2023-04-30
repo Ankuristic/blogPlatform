615260978be313d2baf5c822f355bf0a9701135e
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2023-04-30 09:43:37 +0530

    adding the conroller part

--- controller/blogController.js
+++ controller/blogController.js
@@ -24,9 +24,84 @@ const  createBlogs =  async (req, res) => {
 }
 
 
+
+const getRoutes = async (req, res) => {
+    try {
+      const blogs = await Blogs.find({ isActive: true }) 
+      return res.status(200).json({
+        productsData: products,
+        message: "Products fetched Successfully"
+      });
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ error: err, message: "Internal Server Error" });
+    }
+  };
+// get blogs by id
+ const getBlogById = async (req, res) => {
+    try {
+      const BlogId = req.params.id;
+      const blogs = await Blogs.findById(blogId);
+      if (!blogs) {
+        return res.status(400).json({ message: "blogs Not found" });
+      }
+      return res.status(200).json({
+        product: product,
+        message: "blogs fetched Successfully"
+      });
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ error: err, message: "Internal Server Error" });
+    }
+  };
+
+  // update
+
+const updateBlogs = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const BlogsUpdated = await Blogs.findByIdAndUpdate(id, {
+        ...req.body,
+        updated_by: req.body.userID
+      });
+      if (!BlogsUpdated) {
+        return res
+          .status(400)
+          .json({ message: "Error updating Blogs/Invalid Id" });
+      }
+      return res.status(200).json({ message:"Blogs updated successfully" });
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ error: err, message: "Internal Server Error" });
+    }
+  };
+
+  // delete all blogs
+  const deleteBlogs = async (req, res) => {
+    try {
+      const { id } = req.params;
+      const blogsDeleted = await Blogs.findByIdAndDelete(id);
+      if (!blogsDeleted) {
+        return res
+          .status(400)
+          .json({ message: "Error deleting blogs/Invalid Id" });
+      }
+      return res.status(200).json({ message: "blogs deleted successfully" });
+    } catch (err) {
+      return res
+        .status(500)
+        .json({ error: err, message: "Internal Server Error" });
+    }
+  };
+  
 module.exports ={
     createBlogs,
-   
+    getRoutes,
+    getBlogById,
+    deleteBlogs
 
 
   }
